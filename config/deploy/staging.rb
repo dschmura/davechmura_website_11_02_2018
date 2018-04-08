ded991e1a5addec0f3074208d8aa8e102b01de82
David Chmura <dschmura@humbledaisy.com>
2018-04-08 08:59:47 -0400

    UPDATE: transfer from previous version.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 
-  server "davechmura_website.com", user: 'deployer', roles: %w{app db web}
+  server '159.203.3.130', user: 'deployer', roles: %w{app db web}
   set :stage, :staging
   set :branch, :master
   set :deploy_user, "deployer"
